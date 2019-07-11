25b81d3d49948497982dcdbdbe0342b2f70189af
Jeremy <jeremy.cm7445@gmail.com>
2019-07-10 22:57:04 -0600

    OtroCommitDeJeremy
    Se agrega el test para hacer casos de unidad

---
+++ PruebaCarrito/Test/CasosUnidad.cs
@@ -0,0 +1,29 @@
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+
+
+
+       [Test]
+        public void CompanniaBussinessListarProductos(int indice)
+        {
+           //aqui codigo
+        }
+
+
+    }
+}
